ce09976d1bd6f9827f912ab26f2928f7954e4b9d
Sarran <sarraneymarjr@gmail.com>
2020-02-03 12:01:43 +0530

    changed path

--- server.js
+++ server.js
@@ -36,9 +36,14 @@ app.use('/api/adds',require('./routes/adds'));
 // Production 
 if(process.env.NODE_ENV === 'production'){
   // Set static folder
-  app.use(express.static('./client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'))
+    if(err){
+      res.status(500).send(err);
+    }
+  });
 }
 
 
